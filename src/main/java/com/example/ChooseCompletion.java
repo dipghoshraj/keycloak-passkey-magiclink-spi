ef425fa5934b0eb83ed5456cc8dbb2717eef4e54
dipGhost <deepghosh@pandatoto.in>
2024-06-02 01:22:46 +0530

    feature : adding the base for spi

---
+++ src/main/java/com/example/ChooseCompletion.java
@@ -0,0 +1,72 @@
+package com.example;
+// import java.util.Collections;
+
+import org.keycloak.authentication.RequiredActionContext;
+import org.keycloak.authentication.RequiredActionProvider;
+// import org.keycloak.models.KeycloakSession;
+// import org.keycloak.theme.freemarker.DefaultFreeMarkerProvider;
+import org.keycloak.models.UserModel;
+
+// import java.io.IOException;
+// import java.io.StringWriter;
+// import java.util.HashMap;
+// import java.util.Map;
+import java.util.UUID;
+// import javax.ws.rs.core.Response;
+// import java.util.Locale;
+
+
+
+import org.keycloak.services.messages.Messages;
+// import org.keycloak.theme.Theme;
+// import freemarker.template.Template;
+// import freemarker.template.TemplateException;
+
+
+
+public class ChooseCompletion implements RequiredActionProvider {
+
+    
+    public static final String PROVIDER_ID = "Choose_complition";
+
+
+    @Override
+    public void evaluateTriggers(RequiredActionContext requiredActionContext) {
+
+    }
+
+    @Override
+    public void requiredActionChallenge(RequiredActionContext context) {
+        jakarta.ws.rs.core.Response challenge = context.form().createForm("custom_terms.ftl");
+        context.challenge(challenge);
+
+    }
+
+    @Override
+    public void processAction(RequiredActionContext context) {
+        String selectedMethod = context.getHttpRequest().getDecodedFormParameters().getFirst("completion_method");
+
+        if ("email".equals(selectedMethod)) {
+            UserModel user = context.getUser();
+            user.addRequiredAction(UserModel.RequiredAction.VERIFY_EMAIL);
+            context.success();
+        } else if ("webauthn".equals(selectedMethod)) {
+            context.getUser().addRequiredAction("webauthn-register");
+            context.success();
+        } else if ("magic-link".equals(selectedMethod)) {
+            UserModel user = context.getUser();
+            String magicLinkToken = UUID.randomUUID().toString();
+            context.getAuthenticationSession().setAuthNote("magicLinkToken", magicLinkToken);
+            context.getAuthenticationSession().setAuthNote("magicLinkEmail", user.getEmail());
+            // Here you would send an email with the magic link
+            context.success();
+        } else {
+            context.challenge(context.form().setError(Messages.INVALID_REQUEST).createForm("choose-completion-method.ftl"));
+        }
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
---
+++ src/main/java/com/example/ChooseCompletionFactory.java
@@ -0,0 +1,43 @@
+package com.example;
+
+
+import org.keycloak.Config;
+import org.keycloak.authentication.RequiredActionFactory;
+import org.keycloak.authentication.RequiredActionProvider;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+
+public class ChooseCompletionFactory implements RequiredActionFactory {
+
+    private static final ChooseCompletion SINGLETON = new ChooseCompletion();
+
+    @Override
+    public String getDisplayText() {
+        return "customer webauthn";
+    }
+
+    @Override
+    public RequiredActionProvider create(KeycloakSession keycloakSession) {
+        return SINGLETON;
+    }
+
+    @Override
+    public void init(Config.Scope scope) {
+
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory keycloakSessionFactory) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public String getId() {
+        return "webauthn_magic";
+    }
+}
